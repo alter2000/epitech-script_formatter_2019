caa97ed26133547b5fa7057ff0289c362167dbd5
Jor Bashllari <altnate2000@gmail.com>
2020-01-07 15:56:46 +0100

    basic loop and function REs

--- parse.sh
+++ parse.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
+readonly RE_FUNCTION='(function\s)?[a-zA-Z0-9]+\(\s?\)\s?{.*?}'
+readonly RE_COND='if\s+(.*?)(\s|;)then\s+(.*?)(else)?(.*?)fi'
+readonly RE_FOR='for\s+(.*?)(\s|;)do\s+*(.*?)done'
+readonly RE_WHILE=''
+readonly GREP="grep -Ezo"
 
 parse()
 {
-    grep -E eeeeeeeeeeelp\ besart
     fns="$(parse_functions)"
     loops="$(parse_loops "$fns")"
     conds="$(parse_conds "$loops")"
@@ -11,12 +15,15 @@ parse()
 
 parse_functions()
 {
+    $GREP "$RE_FUNCTION" "$@"
 }
 
 parse_loops()
 {
+    "$GREP" "$RE_LOOP" "$@"
 }
 
 parse_conds()
 {
+    "$GREP" "$RE_COND" "$@"
 }
