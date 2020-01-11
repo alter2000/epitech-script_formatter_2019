1d225be90ed9617c911ce88429e70f7c39269f70
Jor Bashllari <altnate2000@gmail.com>
2020-01-11 17:43:19 +0100

    finish formatting

--- cmds.vim
+++ cmds.vim
@@ -1,27 +1,27 @@
 :if $HEADER != 'none'
-	:normal gg/# "qdapoHEADER"qp
-:endif
+	normal gg/# "qdapo# HEADER"qp
+endif
 
 :if $SPACES == "true"
-	:set noexpandtab
-:else
-	:set expandtab
-:endif
+	set noexpandtab
+else
+	set expandtab
+endif
 
-:let indent = $INDENT
-:set tabstop=indent shiftwidth=indent
+:let g:indent = $INDENT
+:exec 'set tabstop=' . g:indent . ' shiftwidth=' . g:indent
+
+:if $EXPAND
+	%s/\<do\>/\ndo/
+	%s/\<then\>/\nthen/
+	normal gg=G
+endif
 
 :normal gg=G
 
-:if $EXPAND
-	:%s/\<do\>/\ndo/
-	:%s/\<then\>/\nthen/
-	:normal gg=G
-:endif
-
-:normal VG
-:let file = $OUT
-:if ! file
-	:file = '/dev/stdout'
-:endif
-:wq! file
+:let g:file = $OUT
+:if g:file == ''
+	let g:file = '/dev/stdout'
+endif
+:exec 'wq!' . g:file
+:q!
--- script_formatter.sh
+++ script_formatter.sh
@@ -16,75 +16,74 @@ HEADER="none"
 
 main()
 {
-    # -help is speshul
-    for i in "$@"; do
-        if [ "$i" = "-help" ]; then
-            help
-            exit 0
-        fi
-    done
+	# -help is speshul
+	for i in "$@"; do
+		if [ "$i" = "-help" ]; then
+			help
+			exit 0
+		fi
+	done
 
-    TMP="$(getopt -o ":hsi:eo:" \
-        --long "header,spaces,indentation:,expand,output:" -- "$@")"
-    if [ $? != 0 ] ; then
-        echo "getopt failed, exiting" >&2
-        exit 84
-    fi
+	TMP="$(getopt -o ":hsi:eo:" --long "header,spaces,indentation:,expand,output:" -- "$@")"
+	if [ $? != 0 ] ; then
+		echo "getopt failed, exiting" >&2
+		exit 84
+	fi
 
-    eval set -- "$TMP"
+	eval set -- "$TMP"
 
-    while true; do
-        case "$1" in
-            -h|--header)
-                HEADER="$DEFAULT_HEADER"
-                shift
-                ;;
+	while true; do
+		case "$1" in
+			-h|--header)
+				HEADER="$DEFAULT_HEADER"
+				shift
+				;;
 
-            -s|--spaces)
-                SPACES="true"
-                shift
-                ;;
+			-s|--spaces)
+				SPACES="true"
+				shift
+				;;
 
-            -i|--indentation)
-                INDENT="$2"
-                shift 2
-                ;;
+			-i|--indentation)
+				INDENT="$2"
+				shift 2
+				;;
 
-            -e|--expand)
-                EXPAND="true"
-                shift
-                ;;
+			-e|--expand)
+				EXPAND="true"
+				shift
+				;;
 
-            -o|--output)
-                OUT="$2"
-                shift 2
-                ;;
+			-o|--output)
+				OUT="$2"
+				shift 2
+				;;
 
-            -- )
-                shift
-                break
-                ;;
-            *)
-                printf "idk wrong?"
-                echo "$@"
-                break
-                ;;
-        esac
-    done
+			-- )
+				shift
+				break
+				;;
+			*)
+				printf "idk wrong?"
+				echo "$@"
+				break
+				;;
+		esac
+	done
 
-    file="$1"
-    if [ -z "$file" ]; then
-        echo "no file selected" >&2
-        exit 84
-    fi
+	file="$1"
+	if [ -z "$file" ]; then
+		echo "no file selected" >&2
+		exit 84
+	fi
 
-    export SPACES
-    export INDENT
-    export OUT
-    export EXPAND
-    export HEADER
+	export SPACES
+	export INDENT
+	export OUT
+	export EXPAND
+	export HEADER
 
-    vim --clean -E -s cmds.vim "$file"
+	vim --clean -s ./cmds.vim -E "$file"
 }
 
 main "$@"
